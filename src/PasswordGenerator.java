a1b5cfead8001374f6eacf1a1c5974ec556124bb
unknown <tanziulrezaolive1415@gmail.com>
2023-09-22 20:40:54 +0900

    Initial Commit

---
+++ src/PasswordGenerator.java
@@ -0,0 +1,21 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        String password = generatePassword(12); // Change the number to specify the desired password length
+        System.out.println("Generated Password: " + password);
+    }
+
+    public static String generatePassword(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            password.append(characters.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
+}
